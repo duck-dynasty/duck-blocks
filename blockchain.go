b6aaa98f129a115c4187f57cb4cca4379e538905
Thomas Nelson <tn90ca@gmail.com>
2021-11-13 01:23:51 -0500

    flesh out the functionality of transactions, block, and the chain

--- blockchain.go
+++ blockchain.go
@@ -1,17 +1,16 @@
 package main
 
-type Node struct{}
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 type Transaction struct {
 	Sender    string  `json:"sender"`
 	Amount    float32 `json:"amount"`
 	Recipient string  `json:"recipient"`
-}
-
-type Message struct {
-	Sender string `json:"sender"`
-	Text   string `json:"text"`
-	Chat   string `json:"chat"`
 }
 
 type Block struct {
@@ -23,11 +22,40 @@ type Block struct {
 }
 
 type Blockchain struct {
-	Chain               []Block         `json:"chain"`
-	CurrentTransactions []Transaction   `json:"current_transactions"`
-	Nodes               map[string]Node `json:"nodes"`
+	Chain               []Block       `json:"chain"`
+	CurrentTransactions []Transaction `json:"current_transactions"`
 }
 
 func (b *Blockchain) LastBlock() Block {
 	return b.Chain[len(b.Chain)-1]
+}
+
+func (b *Blockchain) NewTransaction(newTransaction Transaction) int {
+	b.CurrentTransactions = append(b.CurrentTransactions, newTransaction)
+
+	return b.LastBlock().Index + 1
+}
+
+func (b *Blockchain) NewBlock(proof string, previousHash string) Block {
+	block := Block{
+		Index:        len(b.Chain) + 1,
+		Proof:        proof,
+		Timestamp:    time.Now().Unix(),
+		Transactions: b.CurrentTransactions,
+		PreviousHash: previousHash,
+	}
+
+	b.CurrentTransactions = nil
+	b.Chain = append(b.Chain, block)
+
+	return block
+}
+
+func Hash(block Block) string {
+	s, _ := json.Marshal(block)
+	s2 := md5.Sum(s)
+
+	d := fmt.Sprintf("%x", s2)
+
+	return d
 }
